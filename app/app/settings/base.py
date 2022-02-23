5be40e12d6df07ba3ba2b14142fc5febf5f67206
vladimir <ducknetica@gmail.com>
2022-02-23 21:42:25 +0300

    вродь более менее с бутстрапом

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'modelcluster',
     'taggit',
 
+    'bootstrap5',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -5,7 +5,7 @@ from .base import *
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 # Database PostgreSQL
 DATABASES = {
     'default': {
